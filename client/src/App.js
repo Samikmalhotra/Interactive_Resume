945b32420b070969a1bde3d18a85b8811833a2b0
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-30 01:29:46 +0530

    added head

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React, {Fragment} from 'react'
 import Navbar from './components/Navbar'
+import Head from './components/Head'
+
+
 
 const App = () => {
   return (
     <Fragment>
       <Navbar/>
+      <Head/>
     </Fragment>
   );
 }
--- client/src/components/Head.js
+++ client/src/components/Head.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Head = () => {
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <a name="top"></a>
+            <div class="parallax">
+                <div data-aos="zoom-out" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                    <h1 class="phead">Samik Malhotra</h1>
+                    <hr class="parallax-hr"/>
+                    <h3 class="phead2">Interactive Resume</h3>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
