54a357662dbc38c9658a6ba3542781b8e085fbf4
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-30 19:22:35 +0530

    added changes to Skills.js

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -3,71 +3,131 @@ import React, { Fragment } from 'react'
 const Skills = () => {
     return (
         <Fragment>
-            <div class="skills">
+            <div className="skills">
     {/* <a name="skills"></a> */}
-    <div class="main">
-      <h1 class="main-heading" data-aos="fade-right" data-aos-duration="1000">Skills</h1>
+    <div className="main">
+      <h1 className="main-heading" data-aos="fade-right" data-aos-duration="1000">Skills</h1>
       <i>
-        <h4 class="subs" data-aos="fade-right" data-aos-duration="1000">"What really matters, is the depth of your heart <br />and the strength of your character"</h4>
+        <h4 className="subs" data-aos="fade-right" data-aos-duration="1000">"What really matters, is the depth of your heart <br />and the strength of your character"</h4>
       </i>
-      <hr class="main-hr"/>
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300">
-            HTML <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+      <hr className="main-hr"/>
+      <div className="row" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300">
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-html5-plain-wordmark colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-css3-plain-wordmark colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-javascript-plain colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-bootstrap-plain-wordmark colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-react-original-wordmark colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-nodejs-plain colored"></i>
+        </div>
+      </div>
+      <div className="row" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="450">
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-redux-original colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-express-original colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-mongodb-plain-wordmark colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-c-plain colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-cplusplus-plain colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-python-plain-wordmark colored"></i>
+        </div>
+      </div>
+      <div className="row" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="600">
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-r-original colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-jquery-plain-wordmark colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-git-plain colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-linux-plain colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-sequelize-plain colored"></i>
+        </div>
+        <div className="col-lg-2 col-md-2 col-sm-3">
+          <i className="devicon-heroku-plain-wordmark colored"></i>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6">
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300">
+            HTML <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="450">
-            CSS <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="450">
+            CSS <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="600">
-            Bootstrap <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="600">
+            Bootstrap <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="750">
-            Javascript <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"/></span><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="750">
+            Javascript <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"/></span><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="900">
-            C++ <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"/></span><i class="fa fa-star"></i><i class="fa fa-star"></i></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="900">
+            C++ <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"/></span><i className="fa fa-star"></i><i className="fa fa-star"></i></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1050">
-            Python <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"/></span><i class="fa fa-star"></i><i class="fa fa-star"></i></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1050">
+            Python <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"/></span><i className="fa fa-star"></i><i className="fa fa-star"></i></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1200">
-            Typography <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span><i class="fa fa-star"></i><i class="fa fa-star"></i></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1200">
+            Typography <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span><i className="fa fa-star"></i><i className="fa fa-star"></i></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1350">
-            Color Theory <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1350">
+            Color Theory <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1500">
-            Growth Mindset<span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1500">
+            Growth Mindset<span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
         </div>
-        <div class="col-lg-6">
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300">
-            User-Interface(UI) <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span><i class="fa fa-star"></i></span>
+        <div className="col-lg-6">
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300">
+            User-Interface(UI) <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span><i className="fa fa-star"></i></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="450">
-            User-Experience(UX) <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"/></span><i class="fa fa-star"></i><i class="fa fa-star"></i></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="450">
+            User-Experience(UX) <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"/></span><i className="fa fa-star"></i><i className="fa fa-star"></i></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="600">
-            Making Mistakes <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="600">
+            Making Mistakes <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="750">
-            Learning from Mistakes <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="750">
+            Learning from Mistakes <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="900">
-            Leadership <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="900">
+            Leadership <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1050">
-            Team-spirit <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1050">
+            Team-spirit <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1200">
-            Adaptability <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1200">
+            Adaptability <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1350">
-            Communication Skills <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1350">
+            Communication Skills <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
-          <div class="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1500">
-            Work-Ethic <span class="rating"><span class="colored-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></span>
+          <div className="col-12" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1500">
+            Work-Ethic <span className="rating"><span className="colored-rating"><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i><i className="fa fa-star"></i></span></span>
           </div>
         </div>
       </div>
